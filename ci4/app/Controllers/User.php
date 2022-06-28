5aaff63b60986b77579c2b2ba4cd5dfa2e9b2345
RezaRiyaldi <rezamoza3@gmail.com>
2022-06-28 12:57:01 +0700

    no

--- ci4/app/Controllers/User.php
+++ ci4/app/Controllers/User.php
@@ -40,6 +40,7 @@ class User extends BaseController
             ];
             return view('user/login', $data);
         }
+        
         $login = $this->user->where('useremail', $email)->first();
         if ($login) {
             $pass = $login['userpassword'];
